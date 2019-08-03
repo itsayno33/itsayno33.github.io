b897c4f6cef27bf3b53a32a9ede12de8ad2a0f11
itsayno33 <53466579+itsayno33@users.noreply.github.com>
2019-08-03 23:40:19 +0900

    Create JSWorks.js

---
+++ test/test/JSWorks.js
@@ -0,0 +1,27 @@
+var resuits = [];
+
+function resultReceiver(event) {
+  results.push(parseInt(event.data));
+  if (results.length ==2) {
+    postMessage(results[0] + results[1]);
+  }
+}
+
+function errorReceiver(event) {
+  throw event.data;
+}
+
+onmessage = function(event) {
+  var n = parseInt(event.data);
+  
+  if (n == 0 || n ==1) {
+    postMessage(n);
+    return;
+  }
+
+for (var i = 1; i <=2; i++) {
+  var worker = new Worker('JSWorks.js');
+  worker.onmessage = resultReceiver;
+  works.onerror = errorReceiber;
+  worker.postMessage( n - 1 );
+}
